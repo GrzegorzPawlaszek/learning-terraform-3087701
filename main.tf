062871cb5adf45294ee506a34da4cc21b3be677a
Grzegorz Pawlaszek <gregpawlaszek@gmail.com>
2022-11-03 12:31:42 +0100

    remove old security group

--- main.tf
+++ main.tf
@@ -47,52 +47,6 @@ module "blog_sg" {
   
   egress_rules        = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-  
-}
-
-# definicja secutiry_group, która będzie działać jako FireWall
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out."
-  
-  # odwołanie się do defaultowej VPC
-  vpc_id = data.aws_vpc.default.id
-}
-
-# reguła na ruch http wchodzący
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  # dozwolony ruch z dowolnego adresu IP
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-}
-
-# reguła na ruch https wchodzący
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-}
-
-# reguła na ruch wychodzący
-resource "aws_security_group_rule" "blog_out" {
-  type        = "egress"
-  from_port   = 80
-  to_port     = 80
-  # dozwolony ruch po wszystkich protokołach
-  protocol    = "-1"
-  # dozwolony ruch na dowolny adres IP
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
 }
 
 
