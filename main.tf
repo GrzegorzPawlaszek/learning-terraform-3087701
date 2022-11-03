d5eea2f44c0e435c8e0609f6a0f741a1980a2e9e
Grzegorz Pawlaszek <gregpawlaszek@gmail.com>
2022-11-03 12:04:08 +0100

    Added security group from module

--- main.tf
+++ main.tf
@@ -24,11 +24,29 @@ resource "aws_instance" "blog" {
   # pobranie typu instancji z pliku variables.tf
   instance_type = var.instance_type
   
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  # podpięcie security_group z modułu. Output dla id securoty group'y jest podany w zakłądce Outputs na stronie registry.terraform.io dla tego modułu
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+# security group zdefiniowana za pomocą predefiniowanego modułu pobranego z registry.terraform.io
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.0"
+  
+  vpc_id = data.aws_vpc.default.id
+  
+  # definicja reguł za pomocą predefiniowanych nazw: https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/rules.tf
+  # to w jaki sposób i co możemy definiować dla modułu jest opisane w sekcji Inputs na stronie registry.terraform.io dla tego modułu
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  egress_rules        = ["all-all"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
 }
 
 # definicja secutiry_group, która będzie działać jako FireWall
